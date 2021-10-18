54cc1ba0a3662ff31a23c6d4cd34483cecbddf9f
lywang1016 <lywang1016@hotmail.com>
2021-10-17 19:49:04 -0700

    player get infomation

--- game_frame_work/player.py
+++ game_frame_work/player.py
@@ -1,14 +1,27 @@
 from deckofcards import Card, Deck
+from evaluation import Evaluation
 
 class Player(object):
     def __init__(self, name, chips):
         self.name = name
         self.chips = chips
-        self.hand = []
         self.flop = []
         self.turn = None
         self.river = None
-        self.all_cards = []
+        self.position = None
+        self.sb = 0
+        self.bb = 0
+        self.pot_size = 0
+        self.chips_to_call = 0
+        self.min_raise = 0
+        self.other_players = []
+        self.others_actions = {}
+        self.my_actions = []
+        self.action_def = {0:"Fold",
+                           1:"Check/Call",
+                           2:"Raise"}
+        self.__hand = []
+        self.__all_cards = []
 
     def say_hello(self):
         print("Hi my name is " + self.name)
@@ -16,43 +29,96 @@ class Player(object):
 
     def show_hand(self):
         print("My hand cards are:")
-        for card in self.hand:
+        for card in self.__hand:
             card.show()
-        return self.hand
+        return self.__hand
     
     def show_chips(self):
         print("I have " + str(self.chips) + " chips")
         return self.chips
 
+    def set_game_size(self, game_size):
+        self.sb = game_size[0]
+        self.bb = game_size[0]
+    
+    def set_your_position(self, position):
+        self.position = position
+        if position == 0:
+            self.chips -= self.sb
+        if position == 1:
+            self.chips -= self.bb
+
+    def other_players_info(self, players):
+        self.other_players = players
+        self.my_actions = []
+        self.others_actions = {}
+        for player in players:
+            if player.position not in self.others_actions:
+                self.others_actions[player.position] = []
+
+    def last_others_action(self, position, action, chips):
+        self.pot_size += chips
+        self.others_actions[position].append([action, chips])
+
+    def win_pot(self):
+        self.chips += self.pot_size
+        self.pot_size = 0
+
+    def show_best_hand(self):
+        evaluate = Evaluation(self.__all_cards)
+        evaluate.get_best_hand()
+        print("My card power is: " + evaluate.power_info[evaluate.max_power])
+        print("My best hand is:")
+        for card in evaluate.best_hand:
+            card.show()
+        return evaluate.best_hand
+    
+    def your_action(self, chips_to_call, min_raise):
+        self.chips_to_call = chips_to_call
+        self.min_raise = min_raise
+
+        if self.chips_to_call = 0:
+            return self.__check()
+        else:
+            return self.__call()
+
     def get_1_card(self, card):
-        if len(self.hand) < 2:
-            self.hand.append(card)
-            self.all_cards.append(card)
+        if len(self.__hand) < 2:
+            self.__hand.append(card)
+            self.__all_cards.append(card)
 
     def get_flop(self, cards):
         self.flop = cards
         for card in cards:
-            self.all_cards.append(card)
+            self.__all_cards.append(card)
 
     def get_turn(self, card):
         self.turn = card
-        self.all_cards.append(card)
+        self.__all_cards.append(card)
     
     def get_river(self, card):
         self.river = card
-        self.all_cards.append(card)
+        self.__all_cards.append(card)
 
-    # def action_fold(self):
-    #     self.hand = []
+    def __fold(self):
+        self.__hand = []
+        self.my_actions.append([0, 0])
+        return [0, 0]
     
-    # def action_check(self):
-    #     self.chips = self.chips
+    def __check(self):
+        self.my_actions.append([1, 0])
+        return [1, 0]
 
-    # def action_call(self, chip_num):
-    #     self.chips -= chip_num
+    def __call(self):
+        self.chips -= self.chips_to_call
+        self.my_actions.append([1, self.chips_to_call])
+        return [1, self.chips_to_call]
 
-    # def action_raise(self, chip_num):
-    #     self.chips -= chip_num
+    def __raise(self):
+        raise_num = self.min_raise
+        self.chips -= raise_num
+        self.my_actions.append(2, raise_num)
+        return [2, raise_num]
 
 if __name__ == '__main__': 
     myDeck = Deck()
